3e87eb887143bdb19160d542f13287a762229f0c
loeli <13250279543@163.com>
2022-10-27 17:49:40 +0800

    'update'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // 本地项目.prettierrc配置测试 确保每个人项目里面的配置都是一样的
 // npx prettier --write app/ 格式化
 export function test(a, b) {
-  const c = 6111;
+  const c = 61114413241234;
   const diff = a - b;
   const count = a + b + c;
   return count;
