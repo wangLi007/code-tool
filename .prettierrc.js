43c688f1646ae531f978270ea6a4a5804fa0b070
loeli <13250279543@163.com>
2022-10-27 16:53:14 +0800

    'update'

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  semi: false, // 语句结尾带分号
-  singleQuote: true, // 单括号
-  bracketSpacing: true, // 大括号内空格
-  printWidth: 100, // 超过最大值换行
-  proseWrap: "preserve",
-};
+  printWidth: 120, // 一行最大多少字符
+  tabWidth: 2, // tab占用的字符数
+  useTabs: false, // 是否使用tab代替空格
+  semi: false, // 是否每句后都加分号
+  singleQuote: true, // 是否使用单引号
+  jsxSingleQuote: false, // jsx是否使用单引号
+  trailingComma: 'all', // 数组尾逗号。
+  bracketSpacing: false, // {foo: xx}还是{ foo: xx }
+  jsxBracketSameLine: false, //看官网
+  arrowParens: 'always', //剪头函数参数是否使用（）
+}
